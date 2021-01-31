4fd01d3eff3acb13be1c8efdfdf601d3dc718387
Thom Bruce <thom@thombruce.com>
2021-01-31 01:44:28 +0000

    add snackbar warning

--- src/views/layouts/Editor.vue
+++ src/views/layouts/Editor.vue
@@ -6,9 +6,25 @@
 
     VMain
       RouterView
+
+    VSnackbar(
+      v-model="snackbar"
+      :timeout="-1"
+      color="error"
+    )
+      | Documents will not be encrypted until you sign in.
+      template(v-slot:action="{ attrs }")
+        VBtn(
+          text
+          v-bind="attrs"
+          @click="snackActive = false"
+        )
+          | Close
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 import VNavBar from '@/components/local/VNavBar.vue'
 import VNavDrawer from '@/components/local/VNavDrawer'
 
@@ -16,6 +32,19 @@ export default {
   components: {
     VNavBar,
     VNavDrawer
+  },
+  data () {
+    return {
+      snackActive: true
+    }
+  },
+  computed: {
+    snackbar () {
+      return this.user ? false : this.snackActive
+    },
+    ...mapState('account', [
+      'user'
+    ])
   }
 }
 </script>
