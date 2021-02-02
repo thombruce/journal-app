66bf97e48296eeb31becd77d20a9b0e5d0deb7e8
Thom Bruce <thom@thombruce.com>
2021-02-02 03:32:33 +0000

    refactor functions

--- src/store/graph/functions.js
+++ src/store/graph/functions.js
@@ -6,60 +6,71 @@ import { user, scope } from '@/gun'
 
 const timestamps = ['createdAt', 'updatedAt']
 
+/* Main Functions */
+
+const debouncedSave = _.debounce(function (document) {
+  save(document)
+}, 500, { maxWait: 2000 })
+
 const save = async function (document) {
-  document = {
+  document = await encryptDocument(document)
+
+  // First, get the document as it exists in graph.
+  user.get(scope)
+    .get('documents')
+    .get(document.id)
+    .once((prev) => {
+      // Then, insert the modified document.
+      user.get(scope)
+        .get('documents')
+        .get(document.id)
+        .put(document, () => {
+          // Retrieve the inserted node
+          const documentNode = user.get(scope)
+            .get('documents')
+            .get(document.id)
+          // Store or update time tree.
+          prev ? updateTimestamps(document, documentNode, prev) : createTimestamps(document, documentNode)
+        })
+    })
+}
+
+/* Support Functions */
+
+const createTimestamps = function (document, documentNode) {
+  // Store node reference in the time tree for createdAt and updatedAt.
+  timestamps.forEach(timestamp => {
+    insertTimestamp(document, documentNode, timestamp)
+  })
+}
+
+const updateTimestamps = function (document, documentNode, prev) {
+  // Nullify the previous updatedAt.
+  insertTimestamp(prev, null, 'updatedAt')
+  // Store node reference in the time tree for new updatedAt.
+  insertTimestamp(document, documentNode, 'updatedAt')
+}
+
+const insertTimestamp = function (document, documentNode, timestamp) {
+  // Store node reference in the time tree for given timestamp.
+  user.get(scope)
+    .get('trees')
+    .get('timestamps')
+    .get(document[timestamp])
+    .get(timestamp)
+    .get(document.id)
+    .put(documentNode)
+}
+
+const encryptDocument = async function (document) {
+  const encrypted = {
     ...document,
     ...{
       content: await SEA.encrypt(document.content, user._.sea),
       text: await SEA.encrypt(document.text, user._.sea)
     }
   }
-
-  // TODO: Refactor - this seems incredibly verbose
-  // First, get the document as it exists in graph.
-  user.get(scope)
-    .get('documents')
-    .get(document.id)
-    .once((prev) => {
-      // If document exists, store previous updatedAt.
-      const previouslyUpdatedAt = prev ? prev.updatedAt : null
-      // Then, insert the modified document.
-      user.get(scope)
-        .get('documents')
-        .get(document.id)
-        .put(document, () => {
-          // Retrieve the newly saved document node.
-          const documentNode = user.get(scope)
-            .get('documents')
-            .get(document.id)
-
-          // Nullify the previous updatedAt.
-          if (previouslyUpdatedAt) {
-            user.get(scope)
-              .get('trees')
-              .get('timestamps')
-              .get(previouslyUpdatedAt)
-              .get('updatedAt')
-              .get(document.id)
-              .put(null)
-          }
-
-          // Store node reference in the time tree for createdAt and updatedAt.
-          timestamps.forEach(timestamp => {
-            user.get(scope)
-              .get('trees')
-              .get('timestamps')
-              .get(document[timestamp])
-              .get(timestamp)
-              .get(document.id)
-              .put(documentNode)
-          })
-        })
-    })
+  return encrypted
 }
-
-const debouncedSave = _.debounce(function (document) {
-  save(document)
-}, 500, { maxWait: 2000 })
 
 export { debouncedSave }
